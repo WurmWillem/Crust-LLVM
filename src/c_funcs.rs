64889cb0dc19a269be64e77dfebabf39b6fff77c
WurmWillem <wurmwillem@proton.me>
2026-04-28 22:49:06 +0200

    changed printing back to green cuz it's pretty

--- src/c_funcs.rs
+++ src/c_funcs.rs
@@ -1,19 +1,24 @@
+use colored::Colorize;
+
 #[unsafe(no_mangle)]
 pub extern "C" fn print_u64(x: u64) {
-    println!("{}", x);
+    let msg = format!("{}", x).green();
+    println!("{}", msg);
 }
 #[unsafe(no_mangle)]
 pub extern "C" fn print_i64(x: i64) {
-    println!("{}", x);
+    let msg = format!("{}", x).green();
+    println!("{}", msg);
 }
 #[unsafe(no_mangle)]
 pub extern "C" fn print_f64(x: f64) {
-    println!("{}", x);
+    let msg = format!("{}", x).green();
+    println!("{}", msg);
 }
 #[unsafe(no_mangle)]
 pub extern "C" fn print_str(ptr: *const u8, len: i64) {
     let slice = unsafe { std::slice::from_raw_parts(ptr, len as usize) };
 
-    let s = std::str::from_utf8(slice).unwrap();
+    let s = std::str::from_utf8(slice).unwrap().green();
     println!("{}", s);
 }
